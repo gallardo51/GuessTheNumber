cbf276b8401ff367f872a98d86e3224cd6ed3713
Alex Sobolev <31108@mail.ru>
2024-04-26 14:34:52 +0300

    Initial Commit

---
+++ GuessTheNumber/GuessTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheNumberApp.swift
+//  GuessTheNumber
+//
+//  Created by Александр Соболев on 26.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
